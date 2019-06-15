647bb00d9bd10047681d827f7fcd5cd3bfcd9bb0
woodpecker <czq.woodpecker@qq.com>
2019-06-15 14:49:05 +0800

    finish Practice04

--- src/main/java/practice04/Person.java
+++ src/main/java/practice04/Person.java
@@ -1,4 +1,30 @@
 package practice04;
 
 public class Person {
+    private String name;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
+
+    public String basicIntroduce() {
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
 }
