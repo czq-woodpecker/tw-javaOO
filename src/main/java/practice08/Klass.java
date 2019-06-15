617df33f065d067a34235793aa05faecf0f744fd
woodpecker <czq.woodpecker@qq.com>
2019-06-15 15:57:28 +0800

    finish Practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,26 @@
 package practice08;
 
 public class Klass {
+    private Integer number;
+    private Student leader;
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public void assignLeader(Student student) {
+        this.leader = student;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public String getDisplayName() {
+        return "Class " + this.number;
+    }
 }
--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,37 @@
 package practice08;
 
 public class Person {
+    private Integer id;
+    private String name;
+    private Integer age;
+
+    public Person() {
+    }
+
+    public Person(Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        return this.id == ((Person)object).getId();
+    }
 }
