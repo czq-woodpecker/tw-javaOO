558899efe6e94a8d215cfb0b828248d5a4f872a4
woodpecker <czq.woodpecker@qq.com>
2019-06-15 15:04:38 +0800

    finish Practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,25 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Integer klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return this.klass == null ?
+                super.introduce() + " I am a Teacher. I teach No Class."
+                : super.introduce() + " I am a Teacher. I teach Class " + this.klass + ".";
+    }
 }
